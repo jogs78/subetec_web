8630b0f61926fc940f1282cbe3bc3646c8cca9d5
Jorge Octavio Guzmán Sánchez <jguzman@ittg.edu.mx>
2026-05-25 00:04:43 -0600

    Subir

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
 
 class UsuarioSeeder extends Seeder
 {
@@ -12,6 +15,27 @@ class UsuarioSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+// Insertamos el usuario con ID = 1 para que coincida perfectamente con el prototipo de Flutter
+        DB::table('usuarios')->updateOrInsert(
+            ['id' => 1], // Si ya existe el ID 1, lo actualiza; si no, lo crea.
+            [
+                'nombre' => 'Jorge Octavio',
+                'correo' => 'jorge.octavio@tuxtla.tecnm.mx',
+                'microsoft_id' => 'ms-mock-oauth2-12345',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        );
+
+        DB::table('usuarios')->updateOrInsert(
+            ['id' => 2], // Si ya existe el ID 2, lo actualiza; si no, lo crea.
+            [
+                'nombre' => 'Fulanito',
+                'correo' => 'fulano@tuxtla.tecnm.mx',
+                'microsoft_id' => 'ms-mock-oauth2-12345',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        );        
     }
 }
